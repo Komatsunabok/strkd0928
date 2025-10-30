9fcda8c764439f8c3ca7518c3ee57639fe08a3ce
Komatsunabok <qron3966@gmail.com>
2025-10-30 18:26:09 +0900

    vuz acc

--- train_model.sh
+++ train_model.sh
@@ -3,7 +3,7 @@ set -e  # ← 途中でエラーが出たら止める（安全）
 
 # === 1つ目 ===
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 10 \
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 100 \
   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
   --distill ckad --layer_usage all --group_num 6 --log_cka 
 echo "=== Done ==="
@@ -11,28 +11,28 @@ sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 # === 1つ目 ===
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 10 \
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 100 \
   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
   --distill ckad --layer_usage key_layers --group_num 6 --log_cka 
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 10 \
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 100 \
   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
   --distill ckad --layer_usage key_layers --group_num 6 --log_cka 
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 10 \
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 100 \
   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
   --distill kd --group_num 6 --log_cka 
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 10 \
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 100 \
   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
   --distill kd --group_num 6 --log_cka 
 echo "=== Done ==="

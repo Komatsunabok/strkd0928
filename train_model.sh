1a167e0534633f98cf038129a953fb05f47dc7a1
Komatsunabok <qron3966@gmail.com>
2025-10-30 11:34:15 +0900

    added command

--- train_model.sh
+++ train_model.sh
@@ -9,6 +9,34 @@ python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
+# === 1つ目 ===
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 10 \
+  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+  --distill ckad --layer_usage key_layers --group_num 6 --log_cka 
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
+
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 10 \
+  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+  --distill ckad --layer_usage key_layers --group_num 6 --log_cka 
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
+
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 10 \
+  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+  --distill kd --group_num 6 --log_cka 
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
+
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 10 \
+  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+  --distill kd --group_num 6 --log_cka 
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 
 

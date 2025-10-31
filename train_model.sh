b5f692eaa6ba8b9a8ea68cf393b284f3c28a9fe4
Komatsunabok <qron3966@gmail.com>
2025-10-31 10:10:16 +0900

    viz acc

--- train_model.sh
+++ train_model.sh
@@ -9,6 +9,13 @@ python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 100 \
+  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+  --distill ckad --layer_usage all --group_num 6 --log_cka 
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
+
 # === 1つ目 ===
 echo "=== Start ==="
 python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 100 \
@@ -31,12 +38,7 @@ python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
-echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 100 \
-  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill kd --group_num 6 --log_cka 
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
+
 
 
 

25dceda9d08f7ac8edadc5a6374efd21616c52e0
Komatsunabok <qron3966@gmail.com>
2025-11-11 14:39:49 +0900

    added commands

--- train_model.sh
+++ train_model.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 set -e  # ← 途中でエラーが出たら止める（安全）
 
+
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 100 --beta_method fixed \
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 300 --beta_method fixed \
   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
   --distill ckad --group_num 6 --layer_usage all --log_cka 
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 100 --beta_method fixed \
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 400 --beta_method fixed \
+  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+  --distill ckad --group_num 6 --layer_usage all --log_cka 
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
+
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn --beta 500 --beta_method fixed \
   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
   --distill ckad --group_num 6 --layer_usage all --log_cka 
 echo "=== Done ==="

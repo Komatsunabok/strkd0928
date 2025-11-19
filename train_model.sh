0189924c32de11e6c3d64f1500ca607b6bcd48dc
Komatsunabok <qron3966@gmail.com>
2025-11-19 18:15:49 +0900

    little change

--- train_model.sh
+++ train_model.sh
@@ -22,32 +22,32 @@ python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
-echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill hint
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
+# echo "=== Start ==="
+# python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
+#   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+#   --distill hint
+# echo "=== Done ==="
+# sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
-echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill hint
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
+# echo "=== Start ==="
+# python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
+#   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+#   --distill hint
+# echo "=== Done ==="
+# sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
-echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill hint
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
+# echo "=== Start ==="
+# python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
+#   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+#   --distill hint
+# echo "=== Done ==="
+# sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
-echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill hint
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
+# echo "=== Start ==="
+# python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
+#   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+#   --distill hint
+# echo "=== Done ==="
+# sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 

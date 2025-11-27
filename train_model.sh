2cf385ee6d6444d68b52c7ede07dcc4bce0a7c75
Komatsunabok <qron3966@gmail.com>
2025-11-28 08:31:14 +0900

    few changes

--- train_model.sh
+++ train_model.sh
@@ -36,24 +36,5 @@ python train_student_resnet.py --dataset cifar100 --model resnet32 --model_t res
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
-echo "=== Start ==="
-python train_student_resnet.py --dataset cifar100 --model resnet32 --model_t resnet32x4 --batch_size 8 -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
-  --distill ckad --group_num 3 --log_cka
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
-echo "=== Start ==="
-python train_student_resnet.py --dataset cifar100 --model resnet32 --model_t resnet32x4 --batch_size 8 -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
-  --distill ckad --group_num 3
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
-
-echo "=== Start ==="
-python train_student_resnet.py --dataset cifar100 --model resnet32 --model_t resnet32x4 --batch_size 8 -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
-  --distill ckad --group_num 3
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
 

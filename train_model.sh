b7e48fb5c7ec2731c55e0820ca1f72d4b0c1a8a3
Komatsunabok <qron3966@gmail.com>
2025-11-28 00:33:45 +0900

    viz cka mat

--- train_model.sh
+++ train_model.sh
@@ -16,19 +16,44 @@ set -e  # ← 途中でエラーが出たら止める（安全）
 # sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill kd --log_cka
+python train_student.py --dataset cifar100 --model resnet32 --model_t resnet32x4 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
+  --distill kd --group_num 3 --log_cka
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model resnet32 --model_t resnet32x4 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
+  --distill kd --group_num 3
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
-# echo "=== Start ==="
-# python train_student.py --dataset cifar100 --model vgg8_bn --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
-#   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-#   --distill ckad --log_cka
-# echo "=== Done ==="
-# sleep 5  # GPUメモリ開放のため5秒待機（任意）
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model resnet32 --model_t resnet32x4 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
+  --distill kd --group_num 3
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model resnet32 --model_t resnet32x4 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
+  --distill ckad --group_num 3 --log_cka
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model resnet32 --model_t resnet32x4 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
+  --distill ckad --group_num 3
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
+
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model resnet32 --model_t resnet32x4 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
+  --distill ckad --group_num 3
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
 

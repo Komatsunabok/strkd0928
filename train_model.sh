054922dc195c91184cd6b29bb03b53de5ec1a618
Komatsunabok <qron3966@gmail.com>
2025-11-28 08:47:33 +0900

    added command

--- train_model.sh
+++ train_model.sh
@@ -16,25 +16,31 @@ set -e  # ← 途中でエラーが出たら止める（安全）
 # sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 echo "=== Start ==="
-python train_student_resnet.py --dataset cifar100 --model resnet32 --model_t resnet32x4 --batch_size 8 -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
-  --distill kd --group_num 3 --log_cka
+python train_student_resnet.py --dataset cifar100 --model resnet14 --model_t resnet14x2 --batch_size 64 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
+  --distill kd --group_num 4 --log_cka
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 echo "=== Start ==="
-python train_student_resnet.py --dataset cifar100 --model resnet32 --model_t resnet32x4 --batch_size 8 -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
-  --distill kd --group_num 3
+python train_student_resnet.py --dataset cifar100 --model resnet14 --model_t resnet14x2 --batch_size 64 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
+  --distill kd --group_num 4 
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 echo "=== Start ==="
-python train_student_resnet.py --dataset cifar100 --model resnet32 --model_t resnet32x4 --batch_size 8 -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t resnet32x4-cifar100-trial_0-epochs_240-bs_64-20251118_165813 \
-  --distill kd --group_num 3
+python train_student_resnet.py --dataset cifar100 --model resnet14 --model_t resnet14x2 --batch_size 64 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
+  --distill ckad --group_num 4 --log_cka
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
+echo "=== Start ==="
+python train_student_resnet.py --dataset cifar100 --model resnet14 --model_t resnet14x2 --batch_size 64 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
+  --distill ckad --group_num 4 
+echo "=== Done ==="
+sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 

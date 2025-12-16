5724333f1c327260f190908ec936ea447c1cfb25
Komatsunabok <qron3966@gmail.com>
2025-12-16 15:40:53 +0900

    added commands

--- train_model.sh
+++ train_model.sh
@@ -19,27 +19,6 @@ set -e  # ← 途中でエラーが出たら止める（安全）
 
 set -e
 
-echo "=== Start ==="
-python train_teacher.py --dataset cifar100 --epochs 240 --trial 0 --model vgg19_bn
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
-
-echo "=== Start ==="
-python train_student.py --dataset cifar100 --model resnet8 --model_t resnet14x2 -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
-  --distill hint
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
-
-echo "=== Start ==="
-python train_teacher.py --dataset cifar100 --epochs 240 --trial 0 --model resnet8
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
-
-echo "=== Start ==="
-python train_teacher.py --dataset cifar100 --epochs 240 --trial 0 --model resnet8
-echo "=== Done ==="
-sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 echo "=== Start ==="
 python train_teacher.py --dataset cifar100 --epochs 240 --trial 0 --model resnet8

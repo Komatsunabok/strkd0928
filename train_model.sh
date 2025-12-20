99a5703b076c330ea98ce08469c5b7809acd0366
Komatsunabok <qron3966@gmail.com>
2025-12-21 00:58:29 +0900

    added commands

--- train_model.sh
+++ train_model.sh
@@ -17,9 +17,33 @@
 # echo "=== Done ==="
 # sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+  --distill ckad --layer_usage key --log_cka 
+echo "=== Done ==="
+
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+  --distill ckad --layer_usage key 
+echo "=== Done ==="
+
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+  --distill ckad --layer_usage key 
+echo "=== Done ==="
 
 echo "=== Start ==="
 python train_student.py --dataset cifar100 --model vgg16_bn --model_t vgg16_bn_half -c 1 -d 1 -b 100 --beta_method fixed \
   --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill ckad --layer_usage key --log_cka 
+  --distill ckad --layer_usage key 
 echo "=== Done ==="
+
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn --model_t vgg16_bn_half -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+  --distill ckad --layer_usage key 
+echo "=== Done ==="
+

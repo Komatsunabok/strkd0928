2a2ee2b1170e69db7e60f3b406c4dabef79282a7
Komatsunabok <qron3966@gmail.com>
2025-12-23 11:42:05 +0900

    added commands

--- train_model.sh
+++ train_model.sh
@@ -18,31 +18,30 @@
 # sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
 echo "=== Start ==="
-python train_teacher.py --dataset cifar100 --epochs 240 --trial 0 --model vgg16_bn
+python train_teacher.py --dataset cifar100 --epochs 240 --trial 0 --model resnet14x2
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 echo "=== Start ==="
-python train_teacher.py --dataset cifar100 --epochs 240 --trial 0 --model vgg16_bn
+python train_teacher.py --dataset cifar100 --epochs 240 --trial 0 --model resnet14x2
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 echo "=== Start ==="
-python train_teacher.py --dataset cifar100 --epochs 240 --trial 0 --model vgg16_bn
+python train_teacher.py --dataset cifar100 --epochs 240 --trial 0 --model resnet14x2
 echo "=== Done ==="
 sleep 5  # GPUメモリ開放のため5秒待機（任意）
 
-
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill ckad --layer_usage all --group_num 6 
+python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14x2 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
+  --distill kd --layer_usage all --group_num 4 
 echo "=== Done ==="
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill ckad --layer_usage all --group_num 6 
+python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14x2 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
+  --distill kd --layer_usage all --group_num 4 
 echo "=== Done ==="
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill ckad --layer_usage all --group_num 6 
+python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14x2 -c 1 -d 1 -b 100 --beta_method fixed \
+  --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
+  --distill kd --layer_usage all --group_num 4 
 echo "=== Done ==="

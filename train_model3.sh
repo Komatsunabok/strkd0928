ab7a764fd5f4443f40e5643a5b9281b0ba2cc1f9
Komatsunabok <qron3966@gmail.com>
2025-12-24 09:43:50 +0900

    added commands

--- train_model3.sh
+++ train_model3.sh
@@ -118,33 +118,49 @@
 #   --distill hint --layer_usage all --group_num 4
 # echo "=== Done ==="
 
-echo "=== Start ==="
-python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14x2 -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
-  --distill ckad --layer_usage all --group_num 4
-echo "=== Done ==="
-echo "=== Start ==="
-python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14x2 -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
-  --distill ckad --layer_usage all --group_num 4
-echo "=== Done ==="
-echo "=== Start ==="
-python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14x2 -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
-  --distill ckad --layer_usage all --group_num 4
-echo "=== Done ==="
+# echo "=== Start ==="
+# python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14x2 -c 1 -d 1 -b 100 --beta_method fixed \
+#   --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
+#   --distill ckad --layer_usage all --group_num 4
+# echo "=== Done ==="
+# echo "=== Start ==="
+# python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14x2 -c 1 -d 1 -b 100 --beta_method fixed \
+#   --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
+#   --distill ckad --layer_usage all --group_num 4
+# echo "=== Done ==="
+# echo "=== Start ==="
+# python train_student.py --dataset cifar100 --model resnet14x2 --model_t resnet14x2 -c 1 -d 1 -b 100 --beta_method fixed \
+#   --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
+#   --distill ckad --layer_usage all --group_num 4
+# echo "=== Done ==="
 
 
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill ckad --layer_usage all --group_num 8
+python train_student.py --dataset cifar100 --model vgg16_bn_half  -c 1 -d 1 -b 100 \ 
+--model_t vgg16_bn --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \ 
+--distill ckad --layer_usage all --group_num 8 --log_cka
 echo "=== Done ==="
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half --model_t vgg16_bn -c 1 -d 1 -b 100 --beta_method fixed \
-  --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-  --distill ckad --layer_usage all --group_num 8
+python train_student.py --dataset cifar100 --model vgg16_bn_half  -c 1 -d 1 -b 100 \ 
+--model_t vgg16_bn --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \ 
+--distill ckad --layer_usage all --group_num 8  
 echo "=== Done ==="
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn_half  -c 1 -d 1 -b 100 \ 
+--model_t vgg16_bn --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \ 
+--distill ckad --layer_usage all --group_num 8  
+echo "=== Done ==="
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn_half  -c 1 -d 1 -b 100 \ 
+--model_t vgg16_bn --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \ 
+--distill ckad --layer_usage all --group_num 8  
+echo "=== Done ==="
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn_half  -c 1 -d 1 -b 100 \ 
+--model_t vgg16_bn --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \ 
+--distill ckad --layer_usage all --group_num 8  
+echo "=== Done ==="
+
 
 # 1221night~
 # echo "=== Start ==="

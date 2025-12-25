a3abc5f860c247a26648dea4e90456faac31e069
Komatsunabok <qron3966@gmail.com>
2025-12-26 00:53:06 +0900

    viz loss and added commands

--- train_model.sh
+++ train_model.sh
@@ -20,15 +20,36 @@
 # for loss viz loss
 
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model vgg16_bn_half  -c 1 -d 1 -b 100 \
+python train_student.py --dataset cifar100 --model vgg16_bn  -c 1 -d 1 -b 1 \
     --model_t vgg16_bn --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-    --distill kd --layer_usage all --group_num 6 --log_cka
+    --distill hint 
 echo "=== Done ==="
+
 
 echo "=== Start ==="
-python train_student.py --dataset cifar100 --model resnet14 -c 1 -d 1 -b 100 --beta_method fixed \
-    --model_t resnet14x2 --model_name_t resnet14x2-cifar100-trial_0-epochs_240-bs_64-20251128_013110 \
-    --distill kd --layer_usage all --group_num 4 --log_cka
+python train_student.py --dataset cifar100 --model vgg16_bn  -c 1 -d 1 -b 1 \
+    --model_t vgg16_bn --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+    --distill hint 
 echo "=== Done ==="
 
+
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn  -c 1 -d 1 -b 1 \
+    --model_t vgg16_bn --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+    --distill hint 
+echo "=== Done ==="
+
+
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn  -c 1 -d 1 -b 1 \
+    --model_t vgg16_bn --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+    --distill hint 
+echo "=== Done ==="
+
+
+echo "=== Start ==="
+python train_student.py --dataset cifar100 --model vgg16_bn  -c 1 -d 1 -b 1 \
+    --model_t vgg16_bn --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
+    --distill hint 
+echo "=== Done ==="
 

f0133b05276edf7a8f1e03ef441f98e5c1aa75cf
Komatsunabok <qron3966@gmail.com>
2025-12-26 00:58:02 +0900

    little

--- train_model.sh
+++ train_model.sh
@@ -22,7 +22,7 @@
 echo "=== Start ==="
 python train_student.py --dataset cifar100 --model vgg16_bn  -c 1 -d 1 -b 1 \
     --model_t vgg16_bn --model_name_t vgg16_bn-cifar100-trial_0-epochs_240-bs_64-20251014_162623 \
-    --distill hint 
+    --distill hint --log_cka
 echo "=== Done ==="
 
 
